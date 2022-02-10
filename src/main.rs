22de30f47fd3ef015c0d569e4f50d7050c39480a
Seldom <HaydenBadgerAZ@gmail.com>
2022-02-10 02:00:00 -0700

    Error handling, removed REPL

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,60 @@
 use std::{
     env::args,
+    error::Error,
+    fmt::{self, Display, Formatter},
     fs::read_to_string,
-    io::{stdin, stdout, Write},
     process::exit,
 };
 
-fn main() {
-    let cmd_args: Vec<String> = args().collect();
+#[derive(Debug)]
+enum CommandError {
+    WrongNumOfArgs,
+}
 
-    if cmd_args.len() > 2 {
-        eprintln!("Usage: tower [script]");
-        exit(1);
-    } else if cmd_args.len() == 1 {
-        let mut input = String::default();
-
-        loop {
-            print!("> ");
-            if let Err(err) = stdout().flush() {
-                eprintln!("Error: {err}");
-                exit(1);
+impl Display for CommandError {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                CommandError::WrongNumOfArgs =>
+                    "wrong number of arguments; expected: tower <script>",
             }
-
-            input.clear();
-            if let Err(err) = stdin().read_line(&mut input) {
-                eprintln!("Error: {err}");
-                exit(1);
-            }
-
-            if input.trim().is_empty() {
-                break;
-            }
-
-            run(&input);
-        }
-    } else if let Some(path) = cmd_args.last() {
-        match read_to_string(path) {
-            Ok(file_text) => run(&file_text),
-            Err(err) => {
-                eprintln!("Error: {err}");
-                exit(1);
-            }
-        }
-        if let Ok(file_text) = read_to_string(path) {
-            run(&file_text);
-        } else {
-            eprintln!("Failed to read file: {}", path);
-            exit(1);
-        }
+        )
     }
 }
 
-fn run(code: &str) {
-    // Token::lex(code).for_each(|token| {
-    //     println!("{:?}", token);
-    // });
+impl Error for CommandError {}
+
+#[derive(Debug)]
+enum LexError {}
+
+impl Display for LexError {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "lexical error")
+    }
+}
+
+impl Error for LexError {}
+
+fn main() {
+    if let Err(err) = run() {
+        eprintln!("Error: {}", err);
+        exit(1);
+    }
+}
+
+fn run() -> Result<(), Box<dyn Error>> {
+    let cmd_args: Vec<String> = args().collect();
+
+    if cmd_args.len() != 2 {
+        Err(Box::new(CommandError::WrongNumOfArgs))
+    } else if let Some(path) = cmd_args.last() {
+        // Token::lex(read_to_string(path)?).for_each(|token| {
+        //     println!("{:?}", token);
+        // });
+        Ok(())
+    } else {
+        unreachable!()
+    }
 }
